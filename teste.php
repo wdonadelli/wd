17e1ec5a26a541c80094e19bc2a047eda9ef7639
wdonadelli <wdonadelli@gmail.com>
2020-05-30 22:40:39 -0300

    Atualizando requisições

--- teste.php
+++ teste.php
@@ -36,16 +36,48 @@ var wdModalClose = function() {console.log("wdOpenClose");}
 
 
 
-function wdStandardRequest(method, action, pack, async, callback) {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//==============================================================================
+
+function wdStandardRequest(action, pack, callback, method, async) {
 
 	/* variáveis locais */
-	var request, data, time, aborted;
+	var request, data, time;
 
-	/*tempo inicial da chamada */
-	time = new Date();
+	/* verificando argumentos */
 
-	/* definindo valor padrão de aborted */
-	aborted = false;
+	if (WD(action).type !== "text") {
+		return false;
+	}
+
+	if (pack === undefined || WD(pack).type === "null") {
+		pack = null;
+	}
+
+	method = (method === undefined || WD(method).type !== "text") ? "GET" : method.toUpperCase();
+
+	if (async === undefined) {
+		async = true;
+	}
+
+	if (WD(callback).type !== "function") {
+		callback = null
+	}
 
 	/* obtendo a interface */
 	if ("XMLHttpRequest" in window) {
@@ -61,47 +93,56 @@ function wdStandardRequest(method, action, pack, async, callback) {
 		return false;
 	}
 
+	/* metodo que verifica se o status já está encerrado */
+	requestClosed = function() {
+		if (["ERROR", "NOTFOUND", "ABORTED", "DONE"].indexOf(data.status) >= 0) {
+			return true;
+		}
+		return false;
+	}
+
 	/* objeto com os dados */
 	data = {
-		status: "UNSENT",     /* UNSENT|OPENED|HEADERS|LOADING|DONE|ABORTED|ERROR */
+		status: "UNSENT",     /* UNSENT|OPENED|HEADERS|LOADING|UPLOADING|DONE|NOTFOUND|ABORTED|ERROR */
 		time: 0,              /* tempo decorrido desde o início da chamada */
-		message: "",          /* registra mensagem do statusText */
 		load: 0,              /* registra o tamanho carregado na requisição */
 		upload: 0,            /* registra o tamanho carregado no upload */
 		TEXT: null,           /* registra o conteúdo textual da requisição */
 		JSON: null,           /* registra o JSON da requisição */
 		XML: null,            /* registra o XML da requisição */
 		abort: function() {   /* registra a função para abortar*/
-			aborted = true;
-			request.abort();
+			if (!requestClosed()) {
+				data.status = "ABORTED";
+				request.abort();
+				if (callback !== null) {
+					callback(data);
+				}
+			}
 			return;
 		}
 	};
 
-	/* abrir janela modal */
-	wdModalOpen();
-
 	/* função a ser executada a cada mudança de estado */
-	request.onreadystatechange = function(x) {
-		if (request.readyState < 1 || request.readyState === 3) {
+	request.onreadystatechange = function(x) {//console.log(request);
+		if (request.readyState < 1 || requestClosed()) {
 			return;
-		} else if (["ERROR", "NOTFOUND", "ABORTED", "DONE"].indexOf(data.status) >= 0) {
-			return;
-		} else if (aborted === true) {
-			data.status = "ABORTED";
-		} else if (request.readyState === 1) {
+		} else if (data.status === "UNSENT") {
 			data.status = "OPENED";
-		} else if (request.readyState > 1 && request.readyState < 4) {
-			if (request.status !== 200 && request.status !== 304) {
-				data.status = "NOTFOUND";
+		} else if (request.status === 404) {
+			data.status = "NOTFOUND";
+		} else if (request.readyState === 2) {
+			data.status = "HEADERS";
+		} else if (request.readyState === 3) {
+			if ("onprogress" in request) {
+				return;
 			} else {
-				data.status = "HEADERS";
+				data.status = "LOADING";
 			}
 		} else if (request.readyState === 4) {
 			if (request.status === 200 || request.status === 304) {
 				data.status = "DONE";
-				data.TEXT  = request.responseText;
-				data.XML   = request.responseXML;
+				data.TEXT   = request.responseText;
+				data.XML    = request.responseXML;
 				try {
 					data.JSON = JSON.parse(data.TEXT) || eval("("+data.TEXT+")");
 				} catch(e) {
@@ -111,78 +152,178 @@ function wdStandardRequest(method, action, pack, async, callback) {
 				data.status = "ERROR";
 			}
 		}
-		data.message = request.statusText === "" ? data.message : request.statusText;
 		data.time = (new Date()) - time;
-		if (["ERROR", "NOTFOUND", "ABORTED", "DONE"].indexOf(data.status) >= 0) {
+		if (requestClosed()) {
 			wdModalClose();
-		}		
-		callback(data);
+		}
+		if (callback !== null) {
+			callback(data);
+		}
 		return;
 	}
 
 	/* função a ser executada durante o progresso */
 	request.onprogress = function(x) {
-		if (["ERROR", "NOTFOUND", "ABORTED", "DONE"].indexOf(data.status) >= 0) {
-			return;
-		} else {
+		if (!requestClosed()) {
 			data.status = "LOADING";
 			data.load = x.loaded;
-			data.message = request.statusText;
 			data.time = (new Date()) - time;
-			callback(data);
+			if (callback !== null) {
+				callback(data);
+			}
 		}
 		return;
 	}
 	
 	/* funções a serem executada durante o upload */
-	request.upload.onloadstart = function(x) {
-		data.upload = x.loaded;
-		data.message = request.statusText;
-		data.time = (new Date()) - time;
-		callback(data);
-		return;
+	if ("upload" in request) {
+		if ("onprogress" in request.upload) {
+			request.upload.onprogress = function(x) {
+				if (!requestClosed()) {
+					data.status = "UPLOADING";
+					data.upload = x.loaded;
+					data.time = (new Date()) - time;
+					if (callback !== null) {
+						callback(data);
+					}
+				}
+				return;
+			}
+		}
 	}
-	request.upload.onprogress = request.upload.onloadstart;
-	//request.upload.onload     = request.upload.onloadstart;
-	//request.upload.onloadend  = request.upload.onloadstart;
+
+	/* abrir janela modal */
+	wdModalOpen();
+
+	/*tempo inicial da chamada */
+	time = new Date();
 
 	/* envio da requisição */
 	request.open(method, action, async);
-	request.send();
+
+	if (method === "POST" && WD(pack).type === "text") {
+		request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	}
+
+	request.send(pack);
 
 	return true;
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//==============================================================================
+
+function testeData() {
+	var data = new FormData();
+	data.append("texto", "willian gostosão");
+	//data.append("arquivo1", wd$("input[name=arquivo2]").item(0).files[0]);
+
+	var text = wd$("form *").form;
+
+	wdStandardRequest("teste.php?nome=valor", data, function(x) {
+		console.log(x);
+		if (x.status === "DONE") {
+			console.log(x.TEXT);
+		}
+		return;
+	}, "POST");
+	return;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function DONE() {
 	console.log("----------------- DONE -----------------");
-	wdStandardRequest("POST", "grande.txt", null, true, function(x) {
+	wdStandardRequest( "grande.txt", null, function(x) {
 		console.log(x);
 		return;
-	});
+	}, "POST", true);
 }
 
 function ABORTED () {
 	console.log("----------------- ABORTED -----------------");
-	wdStandardRequest("POST", "grande.txt", null, true, function(x) {
+	wdStandardRequest("grande.txt", null, function(x) {
 		console.log(x);
-		if (x.time > 31) {
+		//if (x.time > 15) {
+		//if (x.status === "OPENED") {
 			x.abort();
-		}
+//		}
 		return;
-	});
+	}, null, "POST", true);
 }
 
 function NOTFOUND () {
 	console.log("----------------- NOTFOUND -----------------");
-	wdStandardRequest("POST", "grande2.txt", null, true, function(x) {
+	wdStandardRequest("grande2.txt", null, function(x) {
 		console.log(x);
 		return;
-	});
+	}, "POST", true);
 }
+
+function PATH () {
+	console.log("----------------- PATH -----------------");
+	var existe;
+	wdStandardRequest("grande.txt", null, function(x) {
+		existe = x.status === "DONE" ? true : false;
+		return;
+	}, "HEAD", false);
+	console.log(existe ? "Existe o arquivo" : "Não existe o arquivo");
+	return existe;
+}
+
+
+
+
+
+
 
 //DONE();
 //ABORTED();
-NOTFOUND();
+//NOTFOUND();
+//PATH();
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -197,7 +338,7 @@ NOTFOUND();
 		
 	</head>
 
-	<body onload="loko();">
+	<body onload="loko();testeData();">
 
 
 	<form id="loko" class="wd-vform" >
@@ -218,7 +359,10 @@ NOTFOUND();
 
 	<pre>
 <?php
-print_r($_GET);
+print_r($_REQUEST);
+print_r($_FILES);
+
+//move_uploaded_file($_FILES['arquivo1']['tmp_name'], "/home/wd/bundinha2.pdf");
 ?>
 
 	</pre>
