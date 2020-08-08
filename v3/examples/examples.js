4d6937b39d4ac8dc67dc5ae57c0c3421108cfe84
wdonadelli <wdonadelli@gmail.com>
2020-08-08 20:21:17 -0300

    Subindo versão v2 para v3

--- v3/examples/examples.js
+++ v3/examples/examples.js
@@ -4,9 +4,23 @@ function onOffStyle() {
 }
 
 function boxCode(css) {
-	var text;
-	text = css === undefined ? "" : wd$(css).item().innerHTML;
-	wd$("#boxCodeContent").item().textContent = text;
+	var text, tabs, dspl;
+	text = [""];
+	tabs = wd$(css);
+	dspl = tabs.getStyle("display");
+	for (var i = 0; i < dspl.length; i++) {
+		if (dspl[i].toLowerCase() !== "none") {
+			text.push(tabs.item(i).innerHTML);
+		}
+	}
+	wd$("#boxCodeContent").item().textContent = text.join("\n").replace(/\n\t\t\t/g, "\n").trim();
 	wd$("#boxCode").action("open");
 	return;
 }
+
+function setCss(elem, css) {
+	wd$(css).class(null).class({add: elem.value});
+	return;
+}
+
+
