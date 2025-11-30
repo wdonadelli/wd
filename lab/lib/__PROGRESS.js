2541e489f1645620c8425b5052305d0b6248f0af
wd <wdonadelli@github.com>
2025-11-30 00:25:04 -0300

    Atualização

--- lab/lib/__PROGRESS.js
+++ lab/lib/__PROGRESS.js
@@ -7,61 +7,52 @@ const __PROGRESS = {
 
 	/**. '{integer CSS}: Registra o CSS do elemento do módulo.**/
 	CSS: __CSS.data.push(`/*-- PROGRESS --*/
-[data-js-wd-window="progress"] {
-	position: fixed !important;
-	top:   0 !important;
-	right: 0 !important;
-	display: block !important;
-	margin: 0 !important;
-	padding: 0 1em 0 0.5em !important;
-	overflow: hidden !important;
-	z-index: var(--var-js-wd-z-index-0) !important;
-	font-size: var(--var-js-wd-font-size);
-	font-family: var(--var-js-wd-font-type);
-}`)-1,
+.css-wd-progress {
+	padding: 0.5em;
+	border: thin solid black;
+	border-radius: 0.25em;
+	background-color: white;
+}
+.css-wd-progress > progress {
+	width: 100%;
+}`),
 	/**. '{object heap}: Pilha de processos em andamento.**/
 	heap: {},
-	/**. '{node tree}: Elementos da barra de progresso (main, label, bar).**/
-	tree: (function() {
-		const bar   = {tag: "PROGRESS", attr: {id:  "js_wd_window_progress"}};
-		const label = {tag: "LABEL",    attr: {for: bar.attr.id, id: "js_wd_window_progress_percent"}};
-		const attr  = {"data-js-wd-window": "progress", role: "dialog", "aria-labelledby": label.attr.id}
-		const dom   = __DOM({tag: "FORM", attr: attr, child: [bar, label]});
-		return {main: dom.tag, bar: dom.child[0].tag, label: dom.child[1].tag};
-	})(),
+	/**. '{node box}: Container da caixa de progress.**/
+	box:	__DOM({
+		tag: "div",
+		attr: {role: "alert", className: "css-wd-progress"},
+		child: [{tag: "progress", attr: {}, child: []}]
+	}).tag,
 	/**. '{void calc()}: Calcula o valor da barra de progresso.**/
 	calc: function() {
-		let empty = 0;
-		let total = 0;
-		let width = 0;
+		const val = {sum: 0, len: 0};
+		const bar = this.box.firstElementChild;
 		for (let i in this.heap) {
-			width++;
-			empty += this.heap[i] === null ? 1 : 0;
-			total += this.heap[i] === null ? 0 : this.heap[i];
+			if (this.heap[i] === null) {
+				bar.removeAttribute("value");
+				bar.setAttribute("aria-label", "?")
+				return;
+			}
+			val.len++;
+			val.sum += this.heap[i];
 		}
-		if (width === 0) {
-			this.tree.main.remove();
-			this.tree.label.textContent = "?";
-			this.tree.bar.removeAttribute("value");
-		}
-		else if (empty === width) {
-			this.tree.bar.removeAttribute("value");
-			this.tree.label.textContent = "?";
-		}
-		else {
-			const value = total/(width - empty);
-			const text  = value.toLocaleString(__LANG.value, {style: "percent", maximumFractionDigits: 0});
-			this.tree.bar.value       = value;
-			this.tree.label.innerText = text;
-		}
+		const value = val.len === 0 ? 0 : val.sum/val.len;
+		const label = value.toLocaleString(__LANG.value, {style: "percent", maximumFractionDigits: 0});
+		bar.value   = value;
+		bar.setAttribute("aria-label", label);
 		return;
+	},
+	/**. '{boolean empty}: Informa se heap está vazio.**/
+	get empty() {
+		for (let i in this.heap) return false;
+		return true;
 	},
 	/**. '{string open()}: Abre um processo e retorna seu identificador.**/
 	open: function() {
+		if (this.empty) __WINDOW.attach(this.box, "frame");
 		const id = __ID.value;
 		this.heap[id] = null;
-		if (this.tree.main.parentElement !== document.body)
-			document.body.appendChild(this.tree.main);
 		this.calc();
 		return id;
 	},
@@ -71,12 +62,14 @@ const __PROGRESS = {
 			delete this.heap[id];
 			this.calc();
 		}
+		if (this.empty) __WINDOW.detach(this.box);
 		return;
 	},
 	/**. '{void value(string id, float data)}: Define o valor do progresso.**/
 	value: function(id, data) {
 		if (id in this.heap) {
-			this.heap[id] = isFinite(data) ? Math.abs(Number(data)) : null;//FIXME como otimizar isso?
+			const val     = isFinite(data) ? Number(data) : null;
+			this.heap[id] = val === null ? val : (val > 1 ? 1 : (val < 0 ? 0 : val));
 			this.calc();
 		}
 		return;
--- lab/lib/__WINDOW.js
+++ lab/lib/__WINDOW.js
@@ -60,242 +60,286 @@ const __WINDOW = {
 	justify-content: center !important;
 	align-items:     center !important;
 }`),
-	/**. '{array heap}: Registra a fila das janelas modais.**/
+	/**. '{node frame}: Registra a parede para janelas em quadro.**/
+	frame: __HTML("div", {"data-js-wd-window": "frame"}),
+	/**. '{node modal}: Registra a parede para janelas modais.**/
+	modal: __HTML("div", {"data-js-wd-window": "modal"}),
+	/**. '{node float}: Registra a parede para janelas flutuantes.**/
+	float: __HTML("div", {"data-js-wd-window": "float"}),
+	/**. '{array heap}: Registra o histórico de janelas anexadas, cada item é um objeto com as seguintes propriedades:
+	|Nome|Tipo|Descrição|
+	|window|node|Janela a fixar|
+	|wall|string|Tipo de parede|
+	|pin|node ou object|Nó HTML ou posição da tela a anexar o elemento|
+	|status|string|Estado da janela|
+	|focus|node|Valor de '{pin} ou do nó com foco ativo no momento da fixação|**/
 	heap: [],
-	/**. '{object wall}: Registra as paredes fixadoras de janelas.**/
-	wall: Object.freeze({
-		frame: __HTML("div", {"data-js-wd-window": "frame"}),
-		modal: __HTML("div", {"data-js-wd-window": "modal"}),
-		float: __HTML("div", {"data-js-wd-window": "float"})
-	}),
-	/**. '{object data}: Registra os identificadores das janelas fixadas à parede '{data -> ID = data}.**/
-	data: {},
-	/**. '{object find(any key)}: Retorna os dados da janela adicionada localizado pelo identificador ou pela janela.**/
-	find: function(key) {
-		for (let id in this.data)
-			if (key === id || key === this.data[id].window)
-				return this.data[id];
-		return null;
+
+	trash: [],
+	/**. '{boolean check(object data, object item)}: Checa se os dados de '{data} conferem com os dados de '{heap}.**/
+	check: function(data, heap) {
+		let test = false;
+		for (let i in heap) {
+			if (i in data) {
+				if (data[i] !== heap[i]) return false;
+				test = true;
+			}
+		}
+		return test;
 	},
-	/**. '{void fire(string status, object data)}: Dispara eventos de mutação da janela. A propriedade '{detail} contem a informados dos dados informados em '{attach} acrescido do identificador ('{id}) e do '{status}:
+	/**. '{array match(object data)}: Retorna a lista de itens de '{data} que casam com os dados de '{heap}.**/
+	match: function(data) {
+		return this.heap.filter(function(v,i,a) {
+			return this.check(data, v);
+		}, this);
+	},
+	/**. '{integer index(object data)}: Retorna o índice do primeiro item que de '{data} que casa com os dados de '{heap}.**/
+	index: function(data) {
+		for (let i = 0; i < this.heap.length; i++) {
+			if (this.check(data, this.heap[i])) return i;
+		}
+		return -1;
+	},
+	/**. '{boolean inert}: Define a inércia no documento, exceto para a parede "modal".**/
+	set inert(x) {
+		x = x === true;
+		const query = x ? document.body.children : document.querySelectorAll("body > *[inert]");
+		for (let i = 0; i < query.length; i++) {
+			if (query[i] !== this.modal) {
+				if (x) query[i].setAttribute("inert", "true");
+				else   query[i].removeAttribute("inert");
+			}
+		}
+		return;
+	},
+	/**. '{boolean freeze}: Define o congelamento do documento.**/
+	set freeze(x) {
+		x = x === true;
+		__HTML(document.body, {className: x ? {add: "js-wd-freeze"} : {remove: "js-wd-freeze"}});
+		return;
+	},
+	/**. '{void update()}: Define os eventos do documento, o congelamento do documento e a renderização das janelas.**/
+	update: function() {
+		/*-- remover eventos da janela do documento e desligar congelamentos --*/
+		window.removeEventListener("click",   this);
+		window.removeEventListener("keydown", this);
+		window.removeEventListener("resize",  this);
+		/*-- limpar invasores --*/
+		const wall = {frame: this.frame.children, float: this.float.children, modal: this.modal.children};
+		for (let w in wall) {
+			let child = wall[w];
+			for (let i = 0; i < child.length; i++) {
+				if (this.index({window: child[i], wall: w, status: "open"}) < 0)
+					child[i].remove();
+			}
+		}
+		/*-- adicionar janelas --*/
+		this.match({status: "open"}).forEach(function(v,i,a) {
+			if (v.window.parentElement !== this[v.wall])
+				this[v.wall].appendChild(v.window);
+		}, this);
+		/*-- redefinir eventos de janela do documento --*/
+		const float = this.index({wall: "float", status: "open"}) >= 0;
+		const modal = this.index({wall: "modal", status: "open"}) >= 0;
+		const frame = this.index({wall: "frame", status: "open"}) >= 0;
+		if (float) {
+			window.addEventListener("click", this);
+			window.addEventListener("keydown", this);
+			window.addEventListener("resize", this);
+			this.freeze = true;
+			document.body.appendChild(this.float);
+		} else {
+			this.freeze = false;
+			this.float.remove();
+		}
+		if (modal) {
+			this.inert = true;
+			document.body.appendChild(this.modal);
+			if (!float) window.addEventListener("keydown", this);
+		} else {
+			this.inert = false;
+			this.modal.remove();
+		}
+		if (frame) {
+			document.body.appendChild(this.frame);
+		} else {
+			this.frame.remove()
+		}
+		return;
+	},
+	/**. '{void fire(string status, object data)}: Dispara os eventos de mutação da janela ('{wdwindow}) cuja propriedade '{detail} do evento contem os dados informados durante sua anexação acrescido da propriedade '{status}:
 	|Status|Descrição|
 	|avoid|A janela foi rejeitada.|
 	|wait|A janela modal aguarda renderização (está no '{heap}).|
 	|open|A janela foi renderizada.|
 	|close|A janela foi fechada.|
 	|escape|A janela foi descartada.|
+	. Motivos para o status "avoid":
+	- Tentativa de anexação de janela já renderizada;
+	- Anexação de janela "float" ou "frame" com modal aberto;
 	. Motivos para o status "escape":
-	- Botão ESC pressionado com janelas modal ou float aberta;
-	- Clique fora da janela float;
-	- Abertura de uma nova janela float;
-	- Abertura de uma janela modal concomitante a uma janela float aberta; e
-	- Evento de mudança de tamanho da tela concomitante a uma janela float aberta.**/
+	- Botão ESC pressionado com janelas "modal" ou "float" aberta;
+	- Clique fora da janela "float";
+	- Abertura de uma nova janela "float";
+	- Abertura de uma janela "modal" existindo uma janela "float" aberta; e
+	- Evento de mudança de tamanho da tela existindo uma janela "float" aberta.**/
 	fire: function(status, data) {
-		const detail = {wall: data.wall, pin: data.pin, id: data.id, status: status};
-		const event  = new CustomEvent("wdwindow", {detail: detail});
-		data.window.dispatchEvent(event);
-		return;
-	},
-	/**. '{void hide(object data, boolean focus)}: Limpa e esconde a parede.**/
-	hide: function(data, focus) {
-		const kill = {"data-js-wd-window": data.wall, className: null, removeAttribute: ["id"], remove: [], innerHTML: "", style: null};
-		__HTML(this.wall[data.wall], kill);
-		if (focus === true) {
-			try {data.pin.focus();}
-			catch(e) {
-				try {data.active.focus();}
-				catch(f) {}
-			}
+		const item = this.index(data);
+		const heap = this.heap[item];
+		const fire = new CustomEvent("wdwindow", {detail: {wall: heap.wall, pin: heap.pin, status: status}});
+		this.heap[item].status = status;
+		this.update();
+		heap.window.dispatchEvent(fire);
+		/*-- jogando itens descartados para a lixeira: evitar processamento --*/
+		if (status === "avoid" || status === "escape" || status === "close") {
+			this.trash.push(heap);
+			this.heap = this.heap.filter(function(v,i,a) {return v !== heap;}, this);
 		}
 		return;
 	},
-	/**. '{void focus(node win)}: Define o foco da janela renderizada.**/
+	/**. '{void focus(node win)}: Define o foco da janela renderizada na seguinte ordem:
+	- Elemento com atributo '{autofocus};
+	- Primeiro filho da janela; ou
+	- A própria janela.**/
 	focus: function(win) {
 		const query = win.querySelector("[autofocus]");
 		const first = win.childElementCount > 0 ? win.firstElementChild : win;
 		(query !== null ? query.focus() : first.focus());
 		return;
 	},
-	/**. '{boolean inert}: Define a inércia no documento.**/
-	set inert(x) {
-		x = x === true;
-		const query = x ? document.body.children : document.querySelectorAll("body > *[inert]");
-		for (let i = 0; i < query.length; i++) {
-			if (query[i] !== this.wall.modal) {
-				if (x) query[i].setAttribute("inert", "true");
-				else   query[i].removeAttribute("inert");
-			}
-		}
+	/**. '{boolean isNode(any value)}: Checa se o elemento é um nó HTML.**/
+	isNode: function(value) {
+		return value !== null && typeof value === "object" && value instanceof HTMLElement;
 	},
-	/**. '{boolean freeze}: Define o congelamento do documento.**/
-	set freeze(x) {
-		x = x === true;
-		__HTML(document.body, {className: x ? {add: "js-wd-freeze"} : {remove: "js-wd-freeze"}});
+	/**. '{boolean contains(node win)}: Checa se a janela já está contida nas paredes.**/
+	contains: function(win) {
+		return this.match({status: "open"}).filter(function (v,i,a) {
+			return this[v.wall].contains(win);
+		}, this).length > 0;
 	},
-	/**. '{string attach(node win, string wall, any pin)}: Fixa a janela '{win} à parede '{wall} e retorna o '{id} do processo ou nulo.
+	/**. '{void attach(node win, string wall, any pin)}: Renderiza a janela à parede.
 	|Argumento|Tipo|Descrição|
 	|win|node|Janela a fixar, não pode estar contida em outro nó já adicionado.|
 	|wall|string|Nome da parede a fixar a janela: "float", "modal" ou "frame" (padrão)|
-	|pin|node|Nó de fixação da parede "float".|
-	|pin|object|Posição (x, y) da parede "float".|**/
+	|pin|node|Nó de fixação da parede "float" (opcional).|
+	|pin|object|Posição (x, y) da parede "float" (opcional).|**/
 	attach: function(win, wall, pin) {
-		const data = {window: win, wall: wall, pin: pin, id: __ID.value, active: document.activeElement};
 		/*-- janela não é um nó: retornar nulo --*/
-		if (typeof win !== "object" || win === null || !(win instanceof HTMLElement))
-			return null;
-		/*-- janela já adicionada: rejeitar --*/
-		for (let i in this.wall) {
-			if (this.wall[i].contains(win)) {
-				data.id = null;
-				this.fire("avoid", data);
-				return null;
+		if (!this.isNode(win)) throw new TypeError("The window must be an HTML element.");
+		wall = (/^(frame|modal|float)$/i).test(wall) ? String(wall).toLowerCase() : "frame";
+		const focus = this.isNode(pin) ? pin : (document.activeElement !== document.body ? document.activeElement : null);
+		this.heap.push({window: win, wall: wall, pin: pin, focus: focus, status: null});
+		/*-- janela renderizada: rejeitar --*/
+		if (this.contains(win))
+			return this.fire("avoid", {window: win, status: null});
+		/*-- modal aberto: impedir interrupções --*/
+		if (this.index({wall: "modal", status: "open"}) >= 0 && wall !== "modal")
+			return this.fire("avoid", {window: win, status: null});
+		/*-- checar particularidades --*/
+		this.fire("wait", {window: win, status: null});
+		if (wall === "frame") return this.addFrame(win);
+		if (wall === "modal") return this.addModal(win);
+		if (wall === "float") return this.addFloat(win);
+		return;
+	},
+	/**. '{void detach(node win)}: Remove a janela renderizada.**/
+	detach: function(win) {
+		const find = this.match({window: win, status: "open"});
+		if (find.length  === 0       ) return;
+		if (find[0].wall === "frame" ) return this.delFrame(win);
+		if (find[0].wall === "modal" ) return this.delModal(win);
+		if (find[0].wall === "float" ) return this.delFloat(win);
+		return
+	},
+	/**. '{void addModal(node win)}: Adiciona uma janela modal.**/
+	addModal: function(win) {
+		/*-- se já tiver uma janela modal aberta: aguardar --*/
+		if (this.index({wall: "modal", status: "open"}) >= 0) return;
+		/*-- se já tiver uma janela float aberta: remover --*/
+		if (this.index({wall: "float", status: "open"}) >= 0) this.purge("float", true);
+		/*-- adicionar janela --*/
+		this.modal.appendChild(win);
+		win.setAttribute("aria-modal", "true");
+		this.fire("open", {window: win, status: "wait", wall: "modal"});
+		return this.focus(win);
+	},
+	/**. '{void delModal(node win, boolean esc)}: Remove uma janela modal.**/
+	delModal: function(win, esc) {
+		this.match({window: win, status: "open", wall: "modal"}).forEach(function(v,i,a) {
+			v.window.remove();
+			win.removeAttribute("aria-modal");
+			this.fire(esc === true ? "escape" : "close", v);
+			/*-- último item: chamar o próximo modal da fila ou devolver o focus --*/
+			if (i === a.length - 1) {
+				const rest = this.match({status: "wait", wall: "modal"});
+				if (rest.length > 0)
+					this.addModal(rest[0].window);
+				else if	(v.focus !== null)
+					v.focus.focus();
 			}
-		}
-		/*-- analisar dados específicos --*/
-		if (wall === "frame") return this.addFrame(data);
-		if (wall === "modal") return this.addModal(data);
-		if (wall === "float") return this.addFloat(data);
-		return null;
+		}, this);
+		return;
 	},
-	/**. '{string detach(any key)}: Remove a janela atravês do identificador ou pelo nó.**/
-	detach: function(key) {
-		const data = this.find(key);
-		if (data === null)         return null;
-		if (data.wall === "frame") return this.delFrame(data);
-		if (data.wall === "modal") return this.delModal(data);
-		if (data.wall === "float") return this.delFloat(data);
-		return null;
-	},
-	/**. '{string addModal(object data)}: Adiciona uma janela modal e retorna seu identificador ou nulo.**/
-	addModal(data) {
-		/*-- aguardar na fila se já houver um modal aberto --*/
-		if (this.wall.modal.childElementCount > 0) {
-			data.id = null;
-			this.heap.push(data)
-			this.fire("wait", data);
-			return null;
-		}
-		/*-- incompatível com float --*/
-		if (this.wall.float.childElementCount > 0) {
-			const find = this.find(this.wall.float.firstElementChild);
-			if (find === null)
-				this.hide({wall: "float"});
-			else
-				this.delFloat(find, true);
-		}
-		window.addEventListener("keydown", this);
-		this.wall.modal.appendChild(data.window);
-		document.body.appendChild(this.wall.modal);
-		this.inert = true;
-		this.focus(data.window);
-		this.data[data.id] = data;
-		this.fire("open", data);
-		return data.id;
-	},
-	/**. '{string delModal(object data, boolean esc)}: Remove uma janela modal e retorna seu identificador ou nulo.**/
-	delModal(data, esc) {
-		window.removeEventListener("keydown", this);
-		delete this.data[data.id];
-		this.inert = false;
-		this.hide(data, true);
-		this.fire(esc === true ? "escape" : "close", data);
-		if (this.heap.length > 0) {
-			const item = this.heap[0];
-			this.attach(item.window, item.wall, item.pin);
-			this.heap.shift();
-		}
-		return data.id;
-	},
-	/**. '{string addFloat(object data)}: Adiciona uma janela flutuante e retorna seu identificador ou nulo.**/
-	addFloat(data) {
-		/*-- incompatível com modal --*/
-		if (this.wall.modal.childElementCount > 0) {
-			data.id = null;
-			this.fire("avoid", data);
-			return null;
-		}
-		/*-- incompatível com outro float --*/
-		if (this.wall.float.childElementCount > 0) {
-			const find = this.find(this.wall.float.firstElementChild);
-			if (find === null)
-				this.hide({wall: "float"});
-			else
-				this.delFloat(find, true);
-		}
-		window.addEventListener("click", this);
-		window.addEventListener("keydown", this);
-		window.addEventListener("resize", this);
-		this.wall.float.appendChild(data.window);
-		document.body.appendChild(this.wall.float);
-		this.freeze = true;
-		const pin   = new __Pin(this.wall.float, data.pin);
+	/**. '{void addFloat(node win)}: Adiciona uma janela flutuante.**/
+	addFloat: function(win) {
+		/*-- não renderizar dois float ao mesmo tempo --*/
+		this.match({wall: "float", status: "open"}).forEach(function(v,i,a) {
+			this.delFloat(v.window, true);
+		}, this)
+		/*-- adicionar janela --*/
+		this.float.appendChild(win);
+		this.fire("open", {window: win, status: "wait", wall: "float"});
+		/*-- definir posicionamento --*/
+		const data = this.match({window: win, status: "open", wall: "float"})[0];
+		const pin  = new __Pin(this.float, data.pin);
 		pin.fix();
-		this.focus(data.window);
-		this.data[data.id] = data;
-		this.fire("open", data);
-		return data.id;
+		return this.focus(win);
 	},
-	/**. '{string delFloat(object data, boolean esc)}: Remove uma janela flutuante e retorna seu identificador ou nulo.**/
-	delFloat(data, esc) {
-		window.removeEventListener("click", this);
-		window.removeEventListener("keydown", this);
-		window.removeEventListener("resize", this);
-		delete this.data[data.id];
-		this.freeze = false;
-		this.hide(data, true);
-		this.fire(esc === true ? "escape" : "close", data);
-		return data.id;
+	/**. '{void delFloat(node win, boolean esc)}: Remove uma janela flutuante.**/
+	delFloat: function(win, esc) {
+		this.match({window: win, status: "open", wall: "float"}).forEach(function(v,i,a) {
+			v.window.remove();
+			this.fire(esc === true ? "escape" : "close", v);
+			/*-- último item: devolver o focus --*/
+			if (i === a.length - 1 && v.focus !== null)
+				v.focus.focus();
+		}, this);
+		return;
 	},
-	/**. '{string addFrame(object data)}: Adiciona uma janela ao frame e retorna se identificador ou nulo.**/
-	addFrame(data) {
-		this.wall.frame.appendChild(data.window);
-		document.body.appendChild(this.wall.frame);
-		this.data[data.id] = data;
-		this.fire("open", data);
-		return data.id;
+	/**. '{void addFrame(node win)}: Adiciona uma janela ao frame.**/
+	addFrame: function(win) {
+		this.frame.appendChild(win);
+		this.fire("open", {window: win, status: "wait", wall: "frame"});
+		return;
 	},
-	/**. '{string delFrame(object data)}: Remove uma janela do frame e retorna se identificador ou nulo.**/
-	delFrame(data) {
-		data.window.remove();
-		delete this.data[data.id];
-		this.fire("close", data);
-		if (this.wall.frame.childElementCount === 0)
-			this.hide(data, false);
-		return data.id;
+	/**. '{void delFrame(node win, boolean esc)}: Remove uma janela do frame.**/
+	delFrame: function(win, esc) {
+		this.match({window: win, status: "open", wall: "frame"}).forEach(function(v,i,a) {
+			v.window.remove();
+			this.fire(esc === true ? "escape" : "close", v);
+		}, this);
+		return;
+	},
+	/**. '{void purge(string wall)}: Remove todas as janelas renderizadas da parede.**/
+	purge: function(wall) {
+		this.match({wall: wall, status: "open"}).forEach(function(v,i,a) {
+			if      (wall === "frame") this.delFrame(v.window, true);
+			else if (wall === "float") this.delFloat(v.window, true);
+			else if (wall === "modal") this.delModal(v.window, true);
+		}, this);
+		return;
 	},
 	/**. '{void handleEvent(object ev)}: Disparador do objeto chamado durante os eventos '{resize, click e keydown}.**/
 	handleEvent: function(ev) {
-		const float = this.wall.float.firstElementChild;
-		const modal = this.wall.modal.firstElementChild;
-		if (ev.type === "click" && float !== null && !float.contains(ev.target)) {
-			const find = this.find(float);
-			if (find === null)
-				this.hide({wall: "float"});
-			else
-				this.delFloat(find, true);
+		if (ev.type === "click" && !this.float.contains(ev.target)) {
+			this.purge("float");
 		}
-		else if (ev.type === "resize" && float !== null) {
-			const find = this.find(float);
-			if (find === null)
-				this.hide({wall: "float"});
-			else
-				this.delFloat(find, true);
+		else if (ev.type === "resize") {
+			this.purge("float");
 		}
 		else if (ev.type === "keydown" && ev.key === "Escape") {
-			if (float !== null) {
-				const find = this.find(float);
-				if (find === null)
-					this.hide({wall: "float"});
-				else
-					this.delFloat(find, true);
-			}
-			if (modal !== null) {
-				const find = this.find(modal);
-				if (find === null)
-					this.hide({wall: "modal"});
-				else
-					this.delModal(find, true);
-			}
+			this.purge("float");
+			this.purge("modal");
 		}
 		return;
 	},
