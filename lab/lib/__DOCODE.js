affd2581bc004be5f7266919dfa7c4502a1fd1f0
wd <wdonadelli@github.com>
2026-05-20 23:01:12 -0300

    __DOCODE: em aprimoramento

--- lab/lib/__DOCODE.js
+++ lab/lib/__DOCODE.js
@@ -1,63 +1,77 @@
 /**
 #3 Segregando Código
-O objeto '{__DOCODE} segrega as linhas de comentário do código fonte podendo ser traduzido o conteúdo para notação HTML caso seja escrito com determinadas regras de notação.
-**/
+O objeto '{__DOCODE} permite que comentários, ou outras informações delimitadas por caracteres específicos, possam ser segregados do código fonte.
+Também contempla a decodificação de uma linguagem de marcação específica em elementos HTML, permitindo, em conjunto com a ferramenta de segregação, a elaboração de instruções do código dentro do próprio código fonte se utilizando dos comentários.
+
+#4 Regras Gerais
+A notação possui as seguintes regras:
++ Linhas sem caracteres imprimíveis são desconsideradas, exceto dentro de um bloco de texto pré formatado;
++ Cada elemento HTML é identificado por caraceres específicos definidos pela notação;
++ Os caracteres &{amp}, &{lt} e &{gt} são tratados como texto, não sendo sensíveis ao código HTML gerado;
++ Há notação para elementos de bloco e elementos textuais (em linha);
++ Cada notação deve estar separada por quebra de linha, excetos elementos textuais;
++ Blocos de citação e de texto pré formatado podem conter múltiplas linhas;
++ Os caracteres utilizados na notação devem ser informados no início de cada linha seguidos de seu conteúdo;
++ A continuidade de uma determinada notação pressupõe a continuidade do elemento por ela definido;
++ Se não for identificada nenhuma notação, o conteúdo será definido como parágrafo; e
++ Cuidar para que os caracteres delimitadores não estejam contidos em i{strings} ou expressões regulares;
+
+#4 Elementos de Blocos
+- Itens de u{listas não ordenadas} são identificadas pelo caractere &{#x002D} seguido de espaço em branco;
+- Itens de u{listas ordenadas} são identificadas pelo caractere &{#x002B} seguido de espaço em branco;
+- Itens de u{listas de definição} são identificadas pelo caractere &{#x002E} seguido de espaço em branco;
+- Para adicionar um termo à uma u{lista de definição}, deve-se separar o termo da sua descrição com o caractere &{#x003A};
+- u{Tabelas} são identificadas pelo pelo caractere &{#x007C}, utilizado no início, no fim e como separador de colunas;
+- A primeira linha de uma u{tabela} será seu cabeçalho; e
+- u{Títulos} são identificados pelo caractere &{#x0023} seguido do respectivo nível (1-6) e um espaço em branco.
+
+#4 Blocos Multilinhas
+- Um bloco de u{citação} deve iniciar e terminar com um duplo caractere &{#x0022};
+- Um bloco de u{texto pré formatado} deve iniciar e terminar com um duplo caractere &{#x0027}; e
+- Os blocos citados podem conter múltiplas linhas, sendo os caracteres da notação os delimitadores de seu conteúdo.
+
+#4 Elementos Textuais (em linha)
++ Elementos textuais são identificados pelo nome (i{tag}) seguindo de seu conteúdo textual delimitado pelos carateres &{#x007B} e &{#x007D} (span&{#x007B}texto&{#x007D} é igual a <span>texto</span>);
++ Os atributos do elemento textual, se necessário, são informados após o caractere &{#x007D}, da mesma forma ocorrida nos elementos HTML, delimitados pelos caracters &{#x005B} e &{#x005D};
++ Não é possível definir filhos aos elementos textuais;
++ Elementos do tipo bloco também são permitidos, mas podem não fazer sentido;
++ Elemento do tipo '{script} é ignorado, o objetivo da notação é formatação textual; e
++ Os seguintes atalhos podem ser utilizados em substituição ao nome do elemento:
+|Caractere|Descrição|Exemplo|
+|&{#x0027}|Atalho para o elemento '{code}|&{#x0027}&{#x007B}x = 1&{#x007D} e igual à code&{#x007B}x = 1&{#x007D}|
+|&|Atalho para caracteres especiais do tipo i{&#x003B;}|&&{#x007B}#x003B&{#x007D} (sem &{#x003B})|
+[tabela do caralho a quatro]
+
+FIXME falta o menu e o caption
+
+
+#4 Métodos
+O argumento '{body} corresponde ao elemento HTML onde a notação é renderizada.**/
 const __DOCODE = {
-	/**. '{object split(string code, string open, string stop)}: Separa o código conforme caracteres de abertura e fechamento de comentário ('{open/stop}) retornando um objeto contendo informação da fonte ('{src}) e dos comentários ('{doc}).
-	. Os caracteres de abertura e fechamento de comentários não podem estar contidos em strings!**/
-	/**. '{object marks}: Registra as notações da codificação que são analisadas a cada quebra de linha:
-	|Blocos|Notação|
-	|Citação|O caracter &{#x0022} delimita o início e o fim do bloco, as linhas entre os caracteres definirão seu conteúdo.|
-	|Código|O caracter &{#x0027} delimita o início e o fim do bloco, as linhas entre os caracteres definirão seu conteúdo.|
-	|Tabela|Utilize o caracter &{#x007C} como separador de coluna, iniciando e terminando a linha com ele.|
-	|Lista Desordenada|Utilize o caracter &{#x002D} para definir um item desordenado.|
-	|Lista Ordenada|Utilize o caracter &{#x002B} para definir um item ordenado.|
-	|Lista Descritiva|Utilize o caracter &{#x002E} para definir um item descritivo.|
-	|Títulos|Utilize o caracter &{#x0023} seguindo do número (1-6) para definir um título e seu nível.|
-	- Linhas vazias não são consideradas e não interrompem a sequência do bloco;
-	- Os caracteres de abertura e fechamento dos blocos de citação e código não podem conter outros caracteres;
-	- Os caracteres de lista e título devem estar no começo da linha e seguido de um espaço e o seu conteúdo;
-	- Utilize o caracter &{#x003A} para separar o título do item de sua descrição na lista descritiva; e
-	- Se nenhuma notação acima for utilizada, será considerado um parágrafo.**/
-
-	/**. '{node doc(string code, string open, string stop)}`: Retorna os comentários renderizados como documento.**/
-	/**. '{string inline(string inner)}: Retorna o valor de '{innerHTML} para formatar os elementos filhos profundos e possibilitar a definição de elementos i{inline}:
-'
-COM ATRIBUTOS:  tag{texto}[atributo1="valor1" atributo2="valor2"]
-SEM ATRIBUTOS:  tag{texto}
-CODE ABREVIADO: '{texto}
-CARACTERES:     &{code}
-'**/
-
-
-
-
-
-
-
-
-
+	/**. '{object split(string code, string start, string close)}: Segrega os conteúdos delimitados pelos caracteres definidos em '{start} e '{close}, retornando um objeto contendo o conteúdo segregado e o resíduo do conteúdo definido em '{code}:
+	|Nome|Tipo|Descrição|
+	|'{data}|string|Conteúdo segregado|
+	|'{code}|string|Conteúdo original residual com linhas em branco condensadas|**/
 	split: function(code, start, close) {
-		code  = typeof close === "string" ? code.normalize()         : null;
+		code  = typeof code  === "string" ? code.normalize()         : null;
 		start = typeof start === "string" ? start.normalize().trim() : null;
 		close = typeof close === "string" ? close.normalize().trim() : "\n";
 		if (code === null || start === null) return null;
 		const data = [];
-		let init, line, last, find, text, eof = false;
-		while (!eof) {
-			init = code.indexOf(start);
-			line = code.slice(init + start.length).indexOf(close);
-			last = (init + start.length) + (line < 0 ? Infinity : line) + close.length;
-			eof  = init < 0 || line < 0;
-			if (init >= 0) {
-				text = code.slice(init, last);
-				find = text.slice(start.length, line < 0 ? Infinity : text.length - close.length);
-				code = code.replace(text, "");
-				data.push(find);
-			}
+		while (code.indexOf(start) >= 0) {
+			/*-- início da captura --*/
+			let init = code.indexOf(start);
+			let open = init + start.length;
+			/*-- fim da captura --*/
+			let last = code.slice(open).indexOf(close);
+			let stop = last < 0 ? Infinity : open + last;
+			/*-- captura e remoção --*/
+			let find = code.slice(open, stop);
+			let text = start + find + (last < 0 || close === "\n" ? "" : close);
+			code = code.replace(text, "");
+			data.push(find);
 		}
-		data.push(`''${code.replace(/^\s*$/g, "")}''`);
-		this.render(document.body, data.join("\n"))
+		return {code: code.replace(/^\s*$/gm, "").replace(/\n+/g, "\n"), data: data.join("\n")};
 	},
 	/**. '{string inner(string code)}: Decodifica o conteúdo textual para código HTML e o retorna.**/
 	inner: function(code) {
@@ -66,10 +80,11 @@ CARACTERES:     &{code}
 		while(re.test(inner)) {
 			let find = inner.match(re);
 			let attr = find[3] ? find[3] : "";
-			switch(find[1]) {
-				case "'":     inner = inner.replace(find[0], `<code ${attr} translate="no">${find[2]}</code>`); break;
-				case "&amp;": inner = inner.replace(find[0], `&${find[2]};`); break;
-				default:      inner = inner.replace(find[0], `<${find[1]} ${attr}>${find[2]}</${find[1]}>`);
+			switch(find[1].toLowerCase()) {
+				case "'":      inner = inner.replace(find[0], `<code ${attr} translate="no">${find[2]}</code>`); break;
+				case "&amp;":  inner = inner.replace(find[0], `&${find[2]};`); break;
+				case "script": inner = inner.replace(find[0], ""); break;
+				default:       inner = inner.replace(find[0], `<${find[1]} ${attr}>${find[2]}</${find[1]}>`);
 			}
 		}
 		return inner;
@@ -108,6 +123,7 @@ CARACTERES:     &{code}
 		const tbox = elem.tHead === null ? "head" : "body";
 		const trow = document.createElement("tr");
 		/*-- criando containers --*/
+		elem.border = 1;
 		if (elem.tHead === null)       elem.createTHead();
 		if (elem.tBodies.length === 0) elem.createTBody();
 		/*-- capturando células --*/
@@ -178,7 +194,7 @@ CARACTERES:     &{code}
 				if (name === "pre") {
 					node.textContent += `\n${code}`;
 				}
-				else if (name === "blockquote") {
+				else if (name === "blockquote" && code.trim() !== "") {
 					const elem = document.createElement("p");
 					elem.innerHTML = this.inner(code.trim());
 					node.appendChild(elem);
