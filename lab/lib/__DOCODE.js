70375d767596d3bd19371c49eb30cd49e21bb4da
wd <wdonadelli@github.com>
2026-06-05 22:41:16 -0300

    __Tree: excluído; __FORMCREATE: mesclado com __DOCODE e excluído; __DOCODE: ajustes finais em andamento.

--- lab/lib/__DOCODE.js
+++ lab/lib/__DOCODE.js
@@ -1,6 +1,6 @@
 /**
 
-@menu
+@menu{}
 
 #3 Segregando Código
 O objeto '{__DOCODE} permite que comentários, ou outras informações delimitadas por caracteres específicos, possam ser segregados do código fonte.
@@ -8,26 +8,25 @@ Também contempla a decodificação de uma linguagem de marcação específica e
 
 #4 Regras Gerais
 A notação possui as seguintes regras:
-+ Há anotações para elementos contínuos, únicos, de escopo, de formatação textual e especiais e para atributos;
++ Há anotações para elementos contínuos, textuais, de bloco e de escopo e para definição de atributos;
 + Linhas sem caracteres imprimíveis são desconsideradas, exceto dentro do escopo de texto pré formatado;
 + Os caracteres &{amp}, &{lt} e &{gt} são tratados como texto, não sendo sensíveis ao código HTML;
 + A notação é definida por caracteres específicos que identificam o tipo de elemento a ser gerado;
-+ Cada notação deve estar contida numa única linha, excetos os escopos, que aceitam múltiplas linhas;
-+ Os caracteres que abrem ou identificam a notação devem estar no início da linha, exceto a notação para atributos;
-+ Se não for identificada nenhuma notação, o conteúdo será definido como parágrafo; e
++ Cada notação deve estar contida numa única linha, excetos no caso de escopos, que aceitam múltiplas linhas;
++ Se não for identificada nenhuma notação específica, o conteúdo será definido como parágrafo; e
 + Quando a notação estiver compartilhada com o código fonte, os caracteres delimitadores não podem estar contidos em i{strings} ou expressões regulares.
 
 #4 Notação de Atributos
-A notação para atributo estabelece relação com os atributos HTML a serem definidos ao elemento.
-+ Somente os elementos de formatação textual, campos de formulários e especiais permitem definir atributos;
-+ A notação para atributo inicia com o caractere &{#x0040} seguida dos nomes e valores do atributos;
-+ Cada atributo é definido pelo seu nome seguindo do respectivo valor delimitado pelos caracteres &{#x007B} e &{#x007D};
-+ Vários conjuntos de atributos podem ser definidos em sequência;
-+ Nomes de atributos podem conter caracteres alfanumérico, sublinhado e traço apenas; e
+A notação para atributo pode se referir aos atributos HTML do elemento como pode estar vinculada a notações de elementos de bloco.
++ A notação para atributo inicia com o caractere &{#x0040} seguido de uma sequência de nomes e valores;
++ Nomes de atributos podem conter caracteres alfanumérico (minúsculo), sublinhado e traço apenas;
++ Logo após o nome do atributo é definido o seu respectivo valor delimitado pelos caracteres &{#x007B} e &{#x007D};
++ Vários conjuntos de atributos podem ser definidos em sequência, sem espeço entre eles; e
 + Para informar um caractere &{#x007D} como valor do atributo, ele deve ser previamente escapado com &{#x005C}.
 
 #4 Elementos Contínuos
-A continuidade de uma determinada notação pressupõe também a continuidade do seu respectivo elemento por ela representado.
+Nesses elementos, a continuidade de uma determinada notação pressupõe a continuidade do seu respectivo elemento.
+A notação que identifica o elemento deve estar no início da linha.
 
 #5 Listas
 Há notações para listas ordenadas, não ordenadas e de definição:
@@ -46,52 +45,8 @@ No caso de tabelas, há notação para linhas/colunas e para legenda.
 #4 Elementos Únicos
 Elementos únicos são aqueles que não possuem uma continuidade, ocupando a notação de uma única linha.
 
-#5 Títulos/Cabeçalhos
-u{Títulos} são identificados pelo caractere &{#x0023} seguido do nível (1-6), de um espaço em branco e seu conteúdo.
-
-#5 Campos de Formulários
-A notação de formulários está dividida em u{quatro elementos}, todos u{separados por espaço}: o caractere identificador da notação; o rótulo do campo; os atributos do campo; e o valor ou a lista de valores.
-+ Os campos de formulários não precisam estar dentro de um escopo de formulário;
-+ A u{notação} de campo de fomulário é definida pelo caractere &{#x002A} no início da linha;
-+ O u{rótulo} consiste numa i{string} que identifica visualmente o nome do formulário;
-+ Se o campo for de u{preenchimento obrigatório}, o caractere &{#x002A} deve ser adicionado ao fim do rótulo;
-+ A função dos u{atributos} é definir o tipo e nome do campo e outras u{características específicas};
-+ O primeiro atributo deve ser o u{tipo} do campo, incluídos os valores '{textarea} e '{select};
-+ O valor do primeiro atributo (tipo) é o u{nome} do campo;
-+ Após os atributos, é possível definir o u{valor inicial} do campo ou uma u{lista da valores} (para elementos que a comportam);
-+ A u{lista/valor} é delimitada pelos caracteres &{#x005B} e &{#x005D} - múltipla escolha - ou &{#x0028} e &{#x0029} - escolha única;
-+ A definição entre u{múltipla escolha} e u{escolha única} tem relevância apenas para os campos '{select, file, email} e '{radio};
-+ A u{lista/valor} é opcional e seu conteúdo pode ser vazio;
-+ Para definir uma u{lista}, deve-se separar os itens com caractere &{#x002C};
-+ Caso valor e o rótulo do u{item da lista} sejam diferentes, deve-se separá-los com caractere &{#x003A}, nessa ordem; e
-+ Para marcar u{item padrão da lista}, o caractere &{#x002A} deve ser adicionado ao fim de seu rótulo.
-Com as informações da notação, é possível dispensar o preenchimentos dos seguintes atributos: '{required, type, name, value, multiple, list} e '{checked/selected}. Para os demais atributos mais específicos, é preciso definis-lo após o tipo de campo.
-
-
-					UF     @select{uf} [PR,SC*,RS]
-					Nome   @text{nome}required{true} ()
-					Cor    @text{cor} > [Azul, Verde, Vermelho]
-					Aceite @checkbox{ok}actived{true}
-					Idade  @number{idade}min{0}max{80}step{1}
-					OK     @submit.send.formmethod{GET}
-					OK     @type{submit}name{send}formmethod{GET}formaction{loko.php}
-
-					@audio?src{loko.mp3}controls{true}
-					@audio  loko.mp3
-					@video  loko.mpeg
-					@figure loko.jpeg <legenda> <descrição da imagem>
-					@file   loko.xml
-					@menu
-					@figure loko.jpeg "O louco. Pintura de Maluco" "Homem doido sentado de pernas para o ar"
-					! loko.jpeg
-					!	""O louco. Pintura de Maluco""
-					! "Homem doido sentado de pernas para o ar"
-
-
-
-
-
 #4 Elementos de Escopo
+Elementos de escopo aceitam múltiplas linhas. A notação de abertura deve estar no início da linha e a de fechamento no fim da linha.
 - O escopo de u{citação} inicia e termina com um duplo caracter &{#x0022};
 - O escopo de u{texto pré formatado} inicia e termina com um duplo caracter &{#x0027};
 - O escopo de u{formulário} inicia e termina com um duplo caracter &{#x002A};
@@ -101,16 +56,45 @@ Com as informações da notação, é possível dispensar o preenchimentos dos s
 - É possível a um escopo conter um escopo de outro tipo, exceto o de texto pré formatado; e
 - Os escopos de texto pré formatado e citação podem conter conteúdo após ou antes dos caracteres delimitadores, respecitvamente;
 
-#4 Elementos de Formatção Textual (em linha)
-+ Elementos textuais são identificados pelo nome (i{tag} HTML) seguido de seu conteúdo textual delimitado pelos carateres &{#x007B} e &{#x007D};
-+ Os atributos podem são informados após o caractere &{#x007D};
+#4 Elementos Textuais (em linha)
+Os elementos textuais são informados dentro do conteúdo textual e tem por objetivo aplicar determinado comportamento a um fragmento textual.
++ Elementos textuais são identificados pelo respectivo nome (i{tag} HTML) seguido de seu conteúdo textual delimitado pelos carateres &{#x007B} e &{#x007D};
++ Os atributos podem ser definidos posteriormente ao acréscimo do caractere &{#x007D} logo após ao texto (sem espaço);
 + Não é possível definir filhos aos elementos textuais;
-+ Qualquer tipo de elemento, exceto '{script}, pode ser definido, observar o que faz sentido ao conteúdo textual (em linha);
-+ Os seguintes atalhos podem ser utilizados em substituição ao nome do elemento:
++ Qualquer tipo de elemento pode ser definido, exceto '{script}, inclusive elementos de bloco, mesmo não fazendo sentido ao conteúdo; e
++ Os seguintes atalhos podem ser utilizados em substituição ao nome do elemento
 |""Tabela de atalhos para alguns componentes em HTML""|
 |Caractere|Descrição|
 |&{#x0027}|Atalho para o elemento '{code}|
 |&|Atalho para caracteres especiais do tipo i{&#x003B;}|
+
+#4 Elementos de Bloco
+Exceto pelos cabeçalhos, esses elementos são definidos utilizando a notação de atributos, cujo primeiro par nome/valor define o tipo de elemento e uma característica, devendo ser único na linha. Atributos específicos podem existir, não correspondendo necessariamente ao objetivo do atributo HTML de mesmo nome.
+
+#5 Títulos/Cabeçalhos
+u{Títulos} são identificados pelo caractere &{#x0023} seguido do nível (1-6), de um espaço em branco e seu conteúdo.
+
+#5 Campos de Formulários
+
+
+
+A notação de formulários está dividida em u{quatro elementos}, todos u{separados por espaço}: o caractere identificador da notação; o rótulo do campo; os atributos do campo; e o valor ou a lista de valores.
++ Os campos de formulários não precisam estar dentro de um escopo de formulário;
++ A u{notação} de campo de fomulário é definida pelo caractere &{#x002A} no início da linha;
++ O u{rótulo} consiste numa i{string} que identifica visualmente o nome do formulário;
++ Se o campo for de u{preenchimento obrigatório}, o caractere &{#x002A} deve ser adicionado ao fim do rótulo;
++ A função dos u{atributos} é definir o tipo e nome do campo e outras u{características específicas};
++ O primeiro atributo deve ser o u{tipo} do campo, incluídos os valores '{textarea} e '{select};
++ O valor do primeiro atributo (tipo) é o u{nome} do campo;
++ os seguintes atributos não têm efeito em razão de seus valores serem capturados da própria notação ou serem automáticos: '{required, type, name, value, multiple, list, id} e '{checked/selected};
++ Após os atributos, é possível definir o u{valor inicial} do campo ou uma u{lista da valores} (para elementos que a comportam);
++ A u{lista/valor} é delimitada pelos caracteres &{#x005B} e &{#x005D} - múltipla escolha - ou &{#x0028} e &{#x0029} - escolha única;
++ A definição entre u{múltipla escolha} e u{escolha única} tem relevância apenas para os campos '{select, file, email} e '{radio};
++ A u{lista/valor} é opcional e seu conteúdo pode ser vazio;
++ Para definir uma u{lista}, deve-se separar os itens com caractere &{#x003B};
++ Caso valor e o rótulo do u{item da lista} sejam diferentes, deve-se separá-los com caractere &{#x003A}, nessa ordem; e
++ Para marcar u{item padrão da lista}, o caractere &{#x002A} deve ser adicionado ao fim de seu rótulo.
+
 
 #4 Exemplos
 
@@ -214,7 +198,7 @@ const __DOCODE = {
 	|'{html}|string|Codificação do fragmento casado para a notação de atributos HTML|
 	|'{json}|object|Codificação do fragmento casado para objeto|**/
 	attr: function(code) {
-		const attr = /^\@(?:(?:((?:\w|-)+)\{((?:\\\}|[^}])*)\})+)/;
+		const attr = /^\@(?:(?:([a-z0-9_\-]+)\{((?:\\\}|[^}])*)\})+)/;
 		const data = /((?:\w|-)+)\{((?:\\\}|[^}])*)\}/g;
 		const find = code.match(attr);
 		const base = find === null ? "" : find[0].replace(/^\@/, "").replace(/\"/g, `\\"`);
@@ -222,6 +206,7 @@ const __DOCODE = {
 			find: find === null ? "" : find[0],
 			html: base.replace(data, `$1="$2" `).replace(/\\\}/g, "}").trim(),
 			json: JSON.parse("{" + base.replace(data, `"$1": "$2",`).replace(/\\\}/g, "}").replace(/\,$/, "") + "}"),
+			list: JSON.parse("[" + base.replace(data, `{"name": "$1","value":"$2"},`).replace(/\\\}/g, "}").replace(/\,$/, "") + "]"),
 		}
 	},
 	/**. '{string inner(string code)}: Decodifica o conteúdo textual para código HTML e o retorna.**/
@@ -254,37 +239,71 @@ const __DOCODE = {
 		}
 		return last;
 	},
-
-	form: function(body, code) {
-		const re   = /^\s*\*\s*([^@]+)(\@\w+\{.*?\}[^\[\(]*)(\[[^\]]*\]|\([^\)]*\))?\s*$/;
-		const info = /\@(\w+)\{(.*?)\}/;
-		if (!re.test(code)) return false;
-		/*-- registrar --*/
-		const find = code.match(re);
-		const data = {};
-		data.label    = find[1].trim().replace(/\s*\*$/, "*");
-		data.required = data.label.slice(-1) === "*";
-		data.attr     = this.attr(find[2].trim());
-		data.main     = find[3] ? find[3] : "()";
-		data.multiple = data.main[0] === "[";
-		data.info     = find[2].trim().match(info);
-		data.type     = data.info[1] ? data.info[1] : "";
-		data.name     = !data.info[2] || data.info[2].trim() === "" ? label.replace(/\*+$/, "") : data.info[2].trim();
-		data.list     = data.main.slice(1, data.main.length - 1).split(",").map(function(v,i,a) {
-			const item  = v.split(":");
-			const check = v.trim().slice(-1) === "*"
-			return {active: check, value: item[0].trim(), text: item[item.length > 1 ? 1 : 0].trim()};
+	/**. '{node input(node body, object attr)}: Checa, adiciona estrutura de campos de formulários e retorna o resultado.**/
+	input: function(body, attr) {
+		/*-- dados do elemento --*/
+		const tags = {textarea: "textarea", select: "select", button: "button", submit: "button", reset: "button"};
+		const type = "type" in attr.json ? attr.json.type.toLowerCase().trim() : "text";
+		const tag  = type in tags ? tags[type] : "input";
+		const text = attr.list[0].value.trim();
+		const list = !("list" in attr.json) ? [] : attr.json.list.split(",").map(function(v,i,a) {
+			const data       = v.split(":");
+			const item       = document.createElement("option");
+			item.value       = data[0].trim();
+			item.textContent = data.length > 1 ? data[1].trim() : item.value;
+			item.selected    = item.value === String(attr.json.value).trim();
+			return item;
 		});
-		//data.value    =
-		//data.checked  =
-		return data;
+		/*-- eliminando propriedades automáticas --*/
+		delete attr.json[attr.list[0].name];
+		delete attr.json.list;
+		delete attr.json.type;
+
+		/*-- elementos básicos --*/
+		const elem  = document.createElement(tag);
+		const label = document.createElement("label");
+		const span  = document.createElement("span");
+		/*-- propriedades --*/
+		span.textContent = text;
+		label.appendChild(span);
+		if (type !== "textarea" && type !== "select")
+			elem.type = type
+		/*-- botões --*/
+		if (["button", "submit", "reset"].indexOf(type) >= 0) {
+			elem.textContent = text;
+			body.appendChild(elem);
+		}
+		else if (type === "image") {
+			elem.setAttribute("aria-label", text);
+			body.appendChild(elem);
+		}
+		else if (type === "select") {
+			list.forEach(function(v,i,a) {elem.appendChild(v);});
+			label.appendChild(elem);
+			body.appendChild(label);
+		}
+		else if (type === "radio" || type === "checkbox") {
+			label.insertBefore(elem, span);
+			body.appendChild(label);
+		}
+		else {
+			if (list.length > 0 && ["hidden", "password", "color", "textarea"].indexOf(type) < 0) {
+				const data = document.createElement("datalist");
+				list.forEach(function(v,i,a) {data.appendChild(v);});
+				data.id = __ID.value;
+				elem.setAttribute("list", data.id);
+				body.appendChild(data);
+			}
+			label.appendChild(elem);
+			body.appendChild(label);
+		}
+		/*-- definindo atributos --*/
+		for (let i in attr.json)
+			elem.setAttribute(i, attr.json[i]);
+		elem.id = __ID.id(elem);
+		label.setAttribute("for", elem.id);
+		return true;
 	},
-
-
-
-
-
-
 	/**. '{boolean head(node body, string code)}: Checa, adiciona estrutura de títulos e retorna o resultado.**/
 	head: function(body, code) {
 		const re = /^\s*\#([1-6])(.*)$/;
@@ -318,8 +337,11 @@ const __DOCODE = {
 		/*-- registrar --*/
 		const find = code.match(re);
 		const elem = this.create(body, "table");
+		elem.id    = __ID.id(elem);
 		/*-- checando legenda --*/
 		const caption = /^\s*\|\"\"((?:\"[^"]|\"\"[^|]|[^"])+)\"\"\|\s*$/;
+		//FIXME adicionar a um menu de tabelas
+
 		if (caption.test(code)) {
 			const text = code.match(caption)[1].trim();
 			elem.createCaption().innerHTML = this.inner(text);
@@ -373,7 +395,6 @@ const __DOCODE = {
 		return true;
 	},
 	/**. '{boolean scope(node body, string code)}: Checa, adiciona estrutura de blocos e retorna o resultado.**/
-	//FIXME não é possível abri o mesmo bloco dentro dele
 	scope: function(body, code) {
 		/*-- avançando para o último bloco em aberto --*/
 		const open = body.querySelector("[data-wd-notation='1']");
@@ -431,46 +452,50 @@ const __DOCODE = {
 	//FIXME mudar para __DOM e __HTML
 	//FIXME lembrar que var !== null && typeof var === object não é apenas um objeto, pode ser regex
 
-	/**. '{boolean plus(node body, string code)}: Checa, adiciona estruturas especiais e retorna o resultado.**/
-	plus: function(body, code) {
-		const re   = /^\s*\@(\w+)(?:\s+\"([^"]+)\")?(?:\s+\"([^"]+)\")?\s*$/;
-		const file = /([^/\\]+)$/;
-		const find = code.match(re);
-		if (find === null) {
-			return false;
-		}
-		if (find[1].toLowerCase() === "menu") {
+	/**. '{boolean block(node body, string code)}: Checa, adiciona estruturas de bloco e retorna o resultado.**/
+	block: function(body, code) {
+		const attr = this.attr(code.trim());
+		if (attr.list.length === 0) return false;
+		if (attr.list[0].name === "menu") {
 			const elem = document.createElement("menu");
 			body.appendChild(elem);
+			//FIXME adicionar menu tipo tabela e figura
 			return true;
 		}
-		if (find[1].toLowerCase() === "figure" && find[2] !== undefined) {
-			const elem = document.createElement("figure");
-			const img  = __FILE.frame(find[2], find[2].match(file)[1], "image");
-			img.alt = find[3];
-			elem.appendChild(img);
-			body.appendChild(elem);
+		if (attr.list[0].name === "figure") {
+			const figure      = document.createElement("figure");
+			const img         = document.createElement("img");
+			const caption     = document.createElement("figcaption");
+			img.src           = attr.list[0].value;
+			img.alt           = "alt" in attr.json ? attr.json.alt.trim() : "";
+			caption.innerHTML = "caption" in attr.json ? this.inner(attr.json.caption.trim()) : "";
+			figure.id         = __ID.value;
+			//FIXME adicionar a figura ao menu
+			figure.appendChild(img);
+			figure.appendChild(caption);
+			body.appendChild(figure);
 			return true;
 		}
-		if (find[1].toLowerCase() === "file" && find[2] !== undefined && find[3] !== undefined) {
+		if (attr.list[0].name === "audio" || attr.list[0].name === "video") {
+			const midia    = document.createElement(attr.list[0].name);
+			midia.src      = attr.list[0].value;
+			midia.controls = true;
+			body.appendChild(midia);
+			return true;
+		}
+
+		if (attr.list[0].name === "input") {
+			return this.input(body, attr);
+		}
+
+
+
+		/*if (find[1].toLowerCase() === "file" && find[2] !== undefined && find[3] !== undefined) {
 			const elem = __FILE.frame(find[2], find[2].match(file)[1], find[3]);
 			body.appendChild(elem);
 			return true;
-		}
-		if (find[1].toLowerCase() === "caption" && find[2] !== undefined) {
-			const last = body.lastElementChild;
-			const name = last === null ? null : last.tagName.toLowerCase();
-			if (name === "table") {
-				last.createCaption().innerHTML = this.inner(find[2]);
-				return true;
-			}
-			else if (name === "figure") {
-				const elem = document.createElement("figcaption");
-				elem.innerHTML = this.inner(find[2])
-				last.appendChild(elem);
-				return true;
-			}
-		}
+		}*/
+
 		return false;
 	},
 	/**. '{boolean append(node body, string code)}: Checa, adiciona estruturas e retorna o resultado.**/
@@ -480,7 +505,7 @@ const __DOCODE = {
 		if (this.list(body, code))  return true;
 		if (this.table(body, code)) return true;
 		if (this.head(body, code))  return true;
-		if (this.plus(body, code))  return true;
+		if (this.block(body, code)) return true;
 		/*-- parágrafo genérico --*/
 		if (code.trim().length > 0) {
 			const elem = document.createElement("p");
